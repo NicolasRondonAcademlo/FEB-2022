e53f93b843a3a03dec1ab5061ef69a58b66a323e
Nicolas <nicolas.rondon@crehana.com>
2022-08-20 11:39:09 -0500

    a

--- 20-08-2022/ejercicio.py
+++ 20-08-2022/ejercicio.py
@@ -1,11 +1,20 @@
 subjects = ["phy", "chem", "bio"]
 max_score = 300
 class Student:
-    def __init__(self):
-        pass
+    def __init__(self, name, phy, chem, bio):
+        if bio > 100 or phy  > 100 or chem   > 100:
+            raise Exception("El maximo puntaje por materia es 100")
+        self.name = name
+        self.phy = phy
+        self.chem = chem
+        self.bio = bio
 
     def total_obtained(self):
-        pass
+        return self.bio + self.phy + self.chem
 
     def percentage(self):
-        pass
+        return int((self.total_obtained()/300) *100)
+
+maria = Student("maria", 80,50,99)
+print(maria.total_obtained())
+print(maria.percentage())
