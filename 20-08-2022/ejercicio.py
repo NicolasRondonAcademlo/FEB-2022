d7388d7c9cc95369339a1b4f4da1361fcb8187f1
Nicolas <nicolas.rondon@crehana.com>
2022-08-20 11:24:07 -0500

    a

---
+++ 20-08-2022/ejercicio.py
@@ -0,0 +1,11 @@
+subjects = ["phy", "chem", "bio"]
+max_score = 300
+class Student:
+    def __init__(self):
+        pass
+
+    def total_obtained(self):
+        pass
+
+    def percentage(self):
+        pass
--- 20-08-2022/objects.py
+++ 20-08-2022/objects.py
@@ -67,49 +67,91 @@ class MyClassName:
 # print("Name", p2.name)
 # print("Team", p2.team_name)
 
+# class Employee:
+#     def __init__(self, ID=None, salary=None, department=None):
+#         self.ID = ID
+#         self.salary = salary
+#         self.department = department
+    
+#     def tax(self):
+#         return self.salary * 0.2
+    
+#     def salary_per_day(self):
+#         return self.salary / 30
+    
+
+
+
+# steve = Employee(2387, 4800, "Operations")
+# print(steve.salary)
+# print(steve.tax())
+# print(steve.salary_per_day())
+
+# class Doctor:
+#     def __init__(self,
+#                 name, 
+#                 hospital,
+#                 university,
+#                 speciality
+#       ) -> None:
+#         self.name = name
+#         self.speciality =speciality
+#         self.university = university
+#         self.hospital = hospital
+    
+#     def surgery(self):
+#         return f"El medico {self.name} especialista en {self.speciality} esta operando en {self.hospital}"
+
+#     def teaching(self):
+#         return f"{self.name} da clases en {self.university} de {self.speciality}"
+
+# pepito = Doctor(
+#     "pepito",
+#     "HIS",
+#     "UIS",
+#     "Cardiologia"
+# )
+# print(pepito.surgery())
+# print(pepito.teaching())
+
+# class Player:
+#     team_name = "Liverpool"
+    
+#     def __init__(self, name) -> None:
+#         self.name = name
+
+#     @classmethod
+#     def get_team_name(cls):
+#         return cls.team_name
+
+#     @staticmethod
+#     def demo():
+#         return "Static mehtod"
+
+# print(Player.get_team_name())
+# print(Player.demo())
+
+# def funcion_a(funcion_b):
+#     def funcion_c():
+#         print("Anbtes de la funcion")
+#         funcion_b()
+#         print("Despues de la funcion")
+#     return funcion_c
+
+# @funcion_a
+# def saludar():
+#     print("Hola Mundo")
+
+# saludar()
 class Employee:
-    def __init__(self, ID=None, salary=None, department=None):
+    def __init__(self, ID=8, salary=984):
         self.ID = ID
-        self.salary = salary
-        self.department = department
-    
-    def tax(self):
-        return self.salary * 0.2
-    
-    def salary_per_day(self):
-        return self.salary / 30
-    
+        self.__salary = salary
 
+# En python todoo es publico, podemos simular que algo es privado
 
+a = Employee()
 
-steve = Employee(2387, 4800, "Operations")
-print(steve.salary)
-print(steve.tax())
-print(steve.salary_per_day())
-
-class Doctor:
-    def __init__(self,
-                name, 
-                hospital,
-                university,
-                speciality
-      ) -> None:
-        self.name = name
-        self.speciality =speciality
-        self.university = university
-        self.hospital = hospital
-    
-    def surgery(self):
-        return f"El medico {self.name} especialista en {self.speciality} esta operando en {self.hospital}"
-
-    def teaching(self):
-        return f"{self.name} da clases en {self.university} de {self.speciality}"
-
-pepito = Doctor(
-    "pepito",
-    "HIS",
-    "UIS",
-    "Cardiologia"
-)
-print(pepito.surgery())
-print(pepito.teaching())
+print(a.ID)
+#print(a._Employee__salary)
+print(a.__salary)
